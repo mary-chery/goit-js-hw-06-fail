c4deba9625bae144f487491f69a69300c62bb09f
mary-chery <cherkashina.maryna@gmail.com>
2023-02-04 13:00:19 +0200

    hw6

---
+++ js/task-06.js
@@ -0,0 +1,9 @@
+const inputEl = document.querySelector('input#validation-input');
+
+inputEl.addEventListener('blur', onInputBlur);
+
+function onInputBlur (event) {
+    if (inputEl.value.length === Number(inputEl.dataset.length)) {
+        inputEl.classList.add('valid');
+    } inputEl.classList.remove('invalid');
+} 
